cbb9ea271cb7aa21d0742da65fbaab8cd55223a7
easygithdev <easygithdev@gmail.com>
2023-02-06 12:22:41 +0100

    adding test case

---
+++ tests/QueryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use QueryBuilder\Query;
+
+final class QueryTest extends TestCase
+{
+
+
+    public function testFirst(): void
+    {
+        $this->assertEquals(
+            '1',
+            1
+        );
+    }
+
+    public function testSelect(): void
+    {
+
+        $sql =   (new Query)->select()
+            ->from(['authors'])
+            ->toSql();
+            
+        $this->assertEquals(
+            $sql,
+            'SELECT * FROM authors'
+        );
+    }
+}
